b56d7c4be1373f5113c51a7107b29084814fbc07
Elie "Arkh" Harre <68898537+Arkhiv@users.noreply.github.com>
2022-08-21 09:40:07 +0200

    like button start

--- frontend/src/components/Post/LikeButton.jsx
+++ frontend/src/components/Post/LikeButton.jsx
@@ -1,13 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { UidContext } from "../AppContext";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 const LikeButton = ({ post }) => {
   const [liked, setLiked] = useState(false);
+  const uid = useContext(UidContext);
+
+  const like = () => {};
+  const unlike = () => {};
 
   useEffect(() => {
-    if (post.likers.includes(uid))
-  })
+    if (post.likers.includes(uid)) setLiked(true);
+  }, [uid, post.likers, liked]);
 
-  return <div>Likeuuuh</div>;
+  return (
+    <div className="like-container">
+      {uid === null && (
+        <Popup
+          trigger={<img src="./img/icons/heart.svg" alt="like"></img>}
+          position={["bottom center", "bottom right", "bottom left"]}
+          closeOnDocumentClick
+        >
+          <div> Connectez-vous pour aimer un post !</div>
+        </Popup>
+      )}
+      {uid && liked === false && (
+        <img src="./img/icons/heart.svg" alt="like" onClick={like}></img>
+      )}
+      {uid && liked && (
+        <img
+          src="./img/icons/heart-filled.svg"
+          alt="unlike"
+          onClick={unlike}
+        ></img>
+      )}
+    </div>
+  );
 };
 
 export default LikeButton;
